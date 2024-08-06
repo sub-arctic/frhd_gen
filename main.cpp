dae059efa291fbdcc4d95ff13fec391887d2cd02
sub-arctic <mail@subarctic.tech>
2024-08-06 17:00:44 +1000

    added b32e function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 #include <opencv2/opencv.hpp>
 #include <stdexcept>
 
+// convert integer to base32-like format supported by freeriderhd
+std::string frhdEncode(const int& number) {
+    static const char alphabet[] = "0123456789abcdefghijklmnopqrstuv";
+    static const std::size_t maxDigits = 8;
+
+    char buffer[maxDigits + 2]; // +1 for sign, +1 for null terminator
+    char* ptr = buffer + maxDigits + 1; // end of the buffer
+    *ptr = '\0'; // null terminate the string
+    
+    unsigned int absNumber = std::abs(number);
+
+    do {
+	*(--ptr) = alphabet[absNumber % 32]; // corresponding index in char alphabet[]
+	absNumber /= 32; // prepare for next iteration 
+    } while (absNumber > 0); // continue until number > 0
+
+    if (number < 0) {
+	*(--ptr) = '-'; // assign value to buffer and decrement
+    }
+
+    return std::string(ptr);
+}
+
+
 cv::Mat readImage(const std::string& imagePath, const bool& grayscale) {
     cv::Mat image = cv::imread(imagePath, grayscale ? cv::IMREAD_GRAYSCALE : cv::IMREAD_UNCHANGED);
 
